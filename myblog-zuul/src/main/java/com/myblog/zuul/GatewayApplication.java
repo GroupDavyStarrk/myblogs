0eb435903c136b027a2692f7084d38091719fce8
Starrk <ning15845920609@163.com>
2018-01-31 06:50:33 +0800

    1/25

--- myblog-zuul/src/main/java/com/myblog/zuul/GatewayApplication.java
+++ myblog-zuul/src/main/java/com/myblog/zuul/GatewayApplication.java
@@ -2,8 +2,12 @@ package com.myblog.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,5 +15,11 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
+        }
+    @Bean
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    public ZuulProperties zuulProperties(){
+        return  new ZuulProperties();
     }
 }
